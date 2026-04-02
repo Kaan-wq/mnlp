25e161228a7044b49ebebecbc07f782eacdbf0fa
Kaan-wq <kaanucar9@gmail.com>
2026-04-03 01:02:42 +0200

    chore : adapt params

--- transformer/src/config.py
+++ transformer/src/config.py
@@ -4,8 +4,8 @@ from dataclasses import dataclass
 class GPTConfig:
     """Configuration for the GPT model."""
     vocab_size: int = 50257
-    max_seq_length: int = 1024
-    n_embd: int = 768
-    n_layer: int = 12
-    n_head: int = 12
+    max_seq_length: int = 512
+    n_embd: int = 384
+    n_layer: int = 8
+    n_head: int = 8
     dropout: float = 0.1
