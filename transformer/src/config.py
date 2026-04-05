c10a7d316e1b992e8fdb7e37468646bfa1d2fe99
Kaan-wq <kaanucar9@gmail.com>
2026-04-05 02:03:26 +0200

    chore : config update

--- transformer/src/config.py
+++ transformer/src/config.py
@@ -4,9 +4,9 @@ from dataclasses import dataclass
 class GPTConfig:
     """Configuration for the GPT model."""
     vocab_size:     int   = 50257
-    max_seq_length: int   = 256
-    n_embd:         int   = 128
+    max_seq_length: int   = 64
+    n_embd:         int   = 64
     n_layer:        int   = 4
-    n_head:         int   = 4
+    n_head:         int   = 2
     dropout:        float = 0.1
     device:         str   = "cpu"
