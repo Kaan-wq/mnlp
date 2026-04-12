26c4bd280b00d94bc4baa7f54755d35d27c4bf36
Kaan-wq <kaanucar9@gmail.com>
2026-04-12 23:18:25 +0200

    refactor : attention -> mha

--- transformer/src/blocks.py
+++ transformer/src/blocks.py
@@ -1,17 +1,21 @@
 import torch.nn as nn
 import torch.nn.functional as F
 from .config import GPTConfig
-from .attention import MaskedMultiHeadSelfAttention
+from .mha import MaskedMultiHeadSelfAttention
 import torch
+
 
 class MLP(nn.Module):
     def __init__(self, config: GPTConfig) -> None:
         super().__init__()
-        self.fc1 = nn.Linear(config.n_embd, 4 * config.n_embd, device=config.device)
-        self.fc2 = nn.Linear(4 * config.n_embd, config.n_embd, device=config.device)
+        self.fc1 = nn.Linear(config.n_embd, 4 *
+                             config.n_embd, device=config.device)
+        self.fc2 = nn.Linear(
+            4 * config.n_embd, config.n_embd, device=config.device)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         return self.fc2(F.gelu(self.fc1(x)))
+
 
 class TransformerBlock(nn.Module):
     def __init__(self, config: GPTConfig) -> None:
@@ -20,7 +24,7 @@ class TransformerBlock(nn.Module):
         self.ln2 = nn.LayerNorm(config.n_embd, device=config.device)
         self.attn = MaskedMultiHeadSelfAttention(config)
         self.mlp = MLP(config)
-    
+
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         x = x + self.attn(self.ln1(x))
         x = x + self.mlp(self.ln2(x))
