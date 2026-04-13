026861d32344b7f7341e19a6fc22b9e82b9da557
Kaan-wq <kaanucar9@gmail.com>
2026-04-14 01:19:29 +0200

    Update train.py

--- transformer/train.py
+++ transformer/train.py
@@ -37,7 +37,7 @@ def main():
     model_config = GPTConfig(
         max_seq_length=256,
         n_embd=128,
-        n_layer=6,
+        n_layer=80,
         n_head=4,
         attn_type="mha",
     )
