a77910b15204db3fe6d73787450de391ddb79e90
Kaan-wq <kaanucar9@gmail.com>
2026-04-16 19:30:01 +0200

    chore : use cache

--- transformer/generate.py
+++ transformer/generate.py
@@ -73,7 +73,7 @@ def benchmark(
                 max_new_tokens=max_new_tokens,
                 pad_token_id=tokenizer.eos_token_id,
                 do_sample=False,
-                use_cache=False,
+                use_cache=True,
             )
         end.record()
         torch.cuda.synchronize()
